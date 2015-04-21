a2a835bbfa0d9582dc843798076083ee44883b64
Skorney <skorney@list.ru>
2015-04-21 11:15:13 +0300

    progress

--- js/app.js
+++ js/app.js
@@ -5,16 +5,24 @@
         this.device = gem;
 
         this.get_diag_mm = function(){
-            var diagonal_mm = this.device.diagonal_inch * 25.4;
-            this.device.diagonal_mm = diagonal_mm ? diagonal_mm.toFixed(2) : "";
+            var mm = this.device.diagonal_inch * 25.4;
+            this.device.diagonal_mm = mm ? mm.toFixed(2) : "";
+        }
+
+        this.calculate = function() {
+            var ratio = this.device.screen_width/this.device.screen_height;
+            var dev_area = this.device.height*this.device.width;
+            var screen_area = this.device.diagonal_mm*this.device.diagonal_mm*ratio / (1+ratio*ratio);
+            var result = screen_area/dev_area*100;
+            this.device.ratio = result ? result.toFixed(2) : "";
         }
     });
 
     var gem = {
+        ratio: "",
         height: "",
         width: "",
         diagonal_inch: "",
-        diagonal_mm: "",
         screen_height: "",
         screen_width: ""
     };
